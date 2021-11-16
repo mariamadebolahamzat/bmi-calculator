1d958edd902c73ea69c0e8cb550740d63b51edab
Mariam Hamzat <titilayobolamide247@gmail.com>
2021-11-16 10:49:36 +0100

    all implementation done

--- lib/components/reusable_card.dart
+++ lib/components/reusable_card.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({Key? key, this.onPress, this.colour, this.cardChild}) : super(key: key);
+  const ReusableCard({Key? key, required this.onPress, this.colour, this.cardChild}) : super(key: key);
   final Widget? cardChild;
   final Color? colour;
-  final Function? onPress;
+  final Function() onPress;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: onPress,
       child: Container(
         child: cardChild,
         margin: EdgeInsets.all(15.0),
